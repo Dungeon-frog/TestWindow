244b2e5cfe6911e9012658e9dabcb94dc92b539e
comp0713 <comp0713@PC0713>
2022-03-24 10:06:02 +0400

    Добавьте файлы проекта.

---
+++ TestWindow/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZakazLibruary;
+
+namespace TestWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        private ListOrder lo;
+        public MainWindow()
+        {
+            InitializeComponent();
+            lo = new ListOrder();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lo.Add(num.Text);
+            table.Text = lo.updateTable();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void remove_Click(object sender, RoutedEventArgs e)
+        {
+            lo.Delete(num.Text);
+            table.Text = lo.updateTable();
+        }
+
+    }
+}
---
+++ ZakazLibruary/ZakazLibruary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZakazLibruary
+{
+    public class ListOrder
+    {
+        public List<String> table1 = new List<string>();
+
+        public ListOrder()
+        {
+
+        }
+
+        public String updateTable()
+        {
+            String text = "";
+            foreach (var item in table1)
+            {
+                text += item + " ";
+            }
+            return text;
+        }
+
+        public void Add(String val)
+        {
+            table1.Add(val);
+            updateTable();
+        }
+
+        public void Delete(String val)
+        {
+            table1.Remove(val);
+            updateTable();
+        }
+    }
+}
